228aaac83d45f1cd386c78db331aea20934c3631
Guillaume <guitreize@gmail.com>
2009-12-07 18:23:29 +0100

    moved files around for better handling

---
+++ include/bstree.h
@@ -0,0 +1,70 @@
+/*
+ *  GPL
+ *
+ *  Written by Diogo Sousa aka orium
+ *  Copyright (C) 2008 Diogo Sousa
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BSTREE_H_
+#define BSTREE_H_
+
+
+/** 
+ * A bstree node may have:
+ * 	- a parent (if not first node)
+ *  - a right brother
+ *  - a left brother
+ *  - some data
+ */
+struct bstree_node
+{
+	void *data;
+	
+	struct bstree_node *left;
+	struct bstree_node *right;
+	struct bstree_node *parent;
+};
+
+/* Function to add a data in the tree: cmp is the comparison function */
+extern int bstree_add(struct bstree_node **root,
+					  void *data, 
+					  int (*cmp)(const void *, const void *));
+
+/* function that will walk the binary tree from the passed root to realize the action passed in parameter */
+extern void bstree_walk(struct bstree_node *root,
+						void (*action)(void *));
+
+/* Function that will walk the tree from the provided root, but will delete the nodes after each passage.
+ * The free_data function is the function to use to free the data stored in the leaf */
+extern void bstree_free(struct bstree_node *root,
+						void (*free_data)(void *));
+
+/* Function that will search the needle from the root, using the compare function for comparison.
+ * Returns the found data */
+extern void *bstree_search(struct bstree_node *root,
+						   void *needle,
+						   int (*compare)(const void *, const void *));
+
+/* Will wall the tree from the root, looking for the needle.
+ * If found, will use the del function to free the data stored, and 
+ * then delete the node */
+extern int bstree_delete(struct bstree_node **root,
+						 void *needle,
+						 int (*cmp)(const void *, const void *),
+						 void (*del)(void *));
+
+#endif
+
---
+++ src/bstree.c
@@ -0,0 +1,258 @@
+/*
+ *  GPL
+ *
+ *  Written by Diogo Sousa aka orium
+ *  Copyright (C) 2008 Diogo Sousa
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* TODO: 
+ *       * Deletion
+ *       * Rebalancing
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "bstree.h"
+
+/* Declarations */
+static struct bstree_node *alloc_bstree_node(void);
+
+static int bstree_insert(struct bstree_node *,
+						 struct bstree_node *, 
+						 int (*)(const void *, const void *));
+
+static struct bstree_node *bstree_search_recursive(struct bstree_node *root,
+												   void *needle,  
+												   int (*compare)(const void *, const void *));
+
+static struct bstree_node *alloc_bstree_node(void)
+{
+	struct bstree_node *r;
+	
+	r=malloc(sizeof(struct bstree_node));
+	
+	if (r == NULL)
+		abort();
+	
+	return r;
+}
+
+static int bstree_insert(struct bstree_node *tree_node,
+						 struct bstree_node *node, 
+						 int (*cmp)(const void *, const void *))
+{
+	struct bstree_node **dn;
+	int d;
+	
+	d=cmp(node->data,tree_node->data);
+	
+	if (!d)
+		return -1;
+	
+	if (d > 0)
+		dn=&tree_node->right;
+	else
+		dn=&tree_node->left;
+	
+	if (*dn == NULL)
+	{
+		*dn=node;
+		node->left=NULL;
+		node->right=NULL;
+		node->parent = tree_node;
+	}
+	else
+		return bstree_insert(*dn,node,cmp);
+	
+	return 0;
+}
+
+int bstree_add(struct bstree_node **root,
+			   void *data, 
+			   int (*cmp)(const void *, const void *))
+{
+	struct bstree_node *node;
+	
+	if (data == NULL) /* This may be used as a special value */
+		return -1;
+	
+	node=alloc_bstree_node();
+	node->data=data;
+	
+	if (*root == NULL)
+	{
+		*root=node;
+		node->left=NULL;
+		node->right=NULL;
+		node->parent = NULL;
+		return 0;
+	}
+	
+	return bstree_insert(*root,node,cmp);
+}
+
+void bstree_walk(struct bstree_node *node, void (*action)(void *))
+{
+	if (!node)
+		return;
+	if(action)
+		action(node->data);
+	
+	bstree_walk(node->left,action);
+	bstree_walk(node->right,action);
+}
+
+void bstree_free(struct bstree_node *node,
+				 void (*free_data)(void *))
+{
+	if (node == NULL)
+		return;
+	
+	if (free_data != NULL)
+		free_data(node->data);
+	
+	bstree_free(node->left,free_data);
+	bstree_free(node->right,free_data);
+	
+	free(node);
+}
+
+static struct bstree_node *bstree_search_recursive(struct bstree_node *tree_node,
+									 void *needle, 
+									 int (*cmp)(const void *, const void *))
+{
+	struct bstree_node **dn;
+	int d;
+	
+	d=cmp(needle,tree_node->data);
+	
+	if (!d)
+		return tree_node;
+	
+	if (d > 0)
+		dn=&tree_node->right;
+	else
+		dn=&tree_node->left;
+	
+	if (*dn != NULL)
+		return bstree_search_recursive(*dn,needle,cmp);
+	
+	return NULL;
+}
+
+void *bstree_search(struct bstree_node *root,
+					void *needle,
+					int (*cmp)(const void *, const void *))
+{
+	struct bstree_node *found;
+	if (root == NULL)
+		return NULL;
+	
+	found = bstree_search_recursive(root,needle,cmp);
+	if(found)
+		return found->data;
+	else 
+		return NULL;
+}
+
+
+int bstree_delete(struct bstree_node **root,
+				  void *needle,
+				  int (*cmp)(const void *, const void *),
+				  void (*del)(void *))
+{
+	struct bstree_node *toDelete = NULL;
+	
+	if(!root || !*root || !cmp)
+		return 0;
+	
+	toDelete = bstree_search_recursive(*root, needle, cmp);
+	if(toDelete) /* we found the guy */
+	{
+		struct bstree_node *parent = toDelete->parent;
+		/* in case the guy is alone */
+		if(!toDelete->left && !toDelete->right)
+		{
+			/* it has a parent: it is not the root,
+			   but we have to tell his parent that he was freed */
+			if(parent)
+			{
+				if(parent->left == toDelete)
+					parent->left = NULL;
+				else
+					parent->right = NULL;
+			}
+			else /* the guy has no parent: it is the root itself */
+				*root = NULL; /* reset the root */
+
+			/* delete the data it contained */
+			if(del)
+				(del)(toDelete->data);
+			free(toDelete);
+
+		}
+		else if(!toDelete->left)
+		{
+			if(parent)
+			{
+				if(parent->left == toDelete)
+					parent->left = toDelete->right;
+				else
+					parent->right = toDelete->right;
+			}
+			if(del)
+				(del)(toDelete->data);
+			free(toDelete);
+		}
+		else if(!toDelete->right)
+		{
+			if(parent)
+			{
+				if(parent->left == toDelete)
+					parent->left = toDelete->left;
+				else
+					parent->right = toDelete->left;
+			}
+			if(del)
+				(del)(toDelete->data);
+			free(toDelete);
+		}
+		else
+		{
+			/* the guy has 2 children : we don't delete it, we just find a
+			 replacement suitable to replace its value and delete this
+			 "replacement" */
+			struct bstree_node *successor = toDelete->left;
+			struct bstree_node *p = successor;
+			while(p->data)
+			{
+				if(cmp(successor->data,p->data) == 1)
+					successor = p;
+				p = p->left;
+			}
+			/* should have found a possible successor */
+			toDelete->data = successor->data;
+			if(bstree_delete(&toDelete,successor,cmp,del))
+				printf("2 children case: successful");
+		}
+		return 1;
+	}
+	else 
+		return 0;
+	
+}
+
